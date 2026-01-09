b670d728d1f879e009024c2a6418d7c476f21c56
RobinOCraft <robinmoser14@gmail.com>
2026-01-09 22:15:50 +0100

    Add files via upload

---
+++ public/_redirects/Code-component-60-90.tsx
@@ -0,0 +1,5 @@
+# Redirect www to non-www (optional - entfernen falls nicht gewünscht)
+https://www.blockhosts.org/* https://blockhosts.org/:splat 301!
+
+# SPA Routing - alle Routen zu index.html
+/*    /index.html   200
